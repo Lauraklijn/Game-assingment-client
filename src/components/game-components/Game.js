e0e246fc1f5d22ef45cb9a6ebf6eda5b68907c6c
Lauraklijn <lauraklijn12@hotmail.com>
2020-02-21 10:01:48 +0100

    Created game

--- src/components/game-components/Game.js
+++ src/components/game-components/Game.js
@@ -1,23 +1,40 @@
 import React, { Component } from "react";
 import "./Game.css";
+import Board from "./Board";
 
-export default class Game extends Component {
+export default class Game extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Game Page</h1>
-        <div class="board">
-          <div class="square"></div>
-          <div class="square"></div>
-          <div class="square"></div>
-          <div class="square"></div>
-          <div class="square"></div>
-          <div class="square"></div>
-          <div class="square"></div>
-          <div class="square"></div>
-          <div class="square"></div>
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
   }
 }
+
+// export default class Game extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Game Page</h1>
+//         <div class="board">
+//           <div class="square"></div>
+//           <div class="square"></div>
+//           <div class="square"></div>
+//           <div class="square"></div>
+//           <div class="square"></div>
+//           <div class="square"></div>
+//           <div class="square"></div>
+//           <div class="square"></div>
+//           <div class="square"></div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
